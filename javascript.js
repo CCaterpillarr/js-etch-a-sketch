ccc1c914d05868afc08b387191fae8bd52c7437d
CCartepillarr <127199181+CCaterpillarr@users.noreply.github.com>
2023-03-28 15:36:14 +0200

    Create flat size grid in js

--- javascript.js
+++ javascript.js
@@ -1,11 +1,23 @@
 const container = document.querySelector(".container");
 
-const tileSections = [3];
+let size = 3;
 
-tileSections[0] = document.createElement("div");
-tileSections[0].classList.toggle("tileSection");
-container.appendChild(tileSections[0]);
+const tileSections = [];
+const tiles = [];
 
-const tile = document.createElement("div");
-tile.classList.toggle("tile");
-container.appendChild(tile);
+for (let i = 0; i < size; i++){
+    tileSections[i] = document.createElement("div");
+    tileSections[i].classList.toggle("tileSection");
+    container.appendChild(tileSections[i]);
+    
+    for (let j = 0; j < size; j++){
+        tiles[j] = document.createElement("div");
+        tiles[j].classList.toggle("tile");
+        tileSections[i].appendChild(tiles[j]);
+    }
+}
+
+
+
+
+
