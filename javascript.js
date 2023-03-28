682f35f2c0a9519b4877db2c53bd639e00fa1f90
CCartepillarr <127199181+CCaterpillarr@users.noreply.github.com>
2023-03-28 15:09:38 +0200

    Create 1 tile in 1 tile section using array

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
 const container = document.querySelector(".container");
 
-const tileSection = document.createElement("div");
-tileSection.classList.toggle("tileSection");
-container.appendChild(tileSection);
+const tileSections = [3];
+
+tileSections[0] = document.createElement("div");
+tileSections[0].classList.toggle("tileSection");
+container.appendChild(tileSections[0]);
 
 const tile = document.createElement("div");
 tile.classList.toggle("tile");
