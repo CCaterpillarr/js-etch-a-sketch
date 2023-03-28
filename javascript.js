ba094a3b50fecda0890a6d1e152073425d4c02ec
CCartepillarr <127199181+CCaterpillarr@users.noreply.github.com>
2023-03-28 17:55:35 +0200

    Change formatting and comments for readability

--- javascript.js
+++ javascript.js
@@ -1,7 +1,25 @@
+function changeColor(element){
+    element.style.backgroundColor="black";
+    element.style.border = "1px solid rgb(31, 31, 31)"
+}
+
+function createTiles(){
+    const tileSections = [];
+    const tiles = [];
+    for (let i = 0; i < size; i++){  // <size> number of tile sections appear horizontally (cause of display:flex)
+        tileSections[i] = document.createElement("div");
+        container.appendChild(tileSections[i]);
+        for (let j = 0; j < size; j++){  // <size> number of tiles appear vertically in each tile section
+            tiles[j] = document.createElement("div");
+            tiles[j].classList.toggle("tile");  // This class gives them size and gray background
+            tiles[j].setAttribute("onmouseover", "changeColor(this)");
+            tileSections[i].appendChild(tiles[j]);
+        }
+    }
+}
+
 function changeSize(){
-
     let userInputSize = prompt("Enter new size (max size - 100):");
-
      if(isNaN(userInputSize)){
         alert("Not a valid size");
      } else if (Number(userInputSize) > 100) {
@@ -13,42 +31,15 @@ function changeSize(){
 }
 
 function reset(){
+    
     while (container.lastChild != null){
         container.removeChild(container.lastChild);
     }
     
-    draw();
+    createTiles();
 }
 
-function draw(){
-    for (let i = 0; i < size; i++){
-        tileSections[i] = document.createElement("div");
-        tileSections[i].classList.toggle("tileSection");
-        container.appendChild(tileSections[i]);
-        for (let j = 0; j < size; j++){
-            tiles[j] = document.createElement("div");
-            tiles[j].classList.toggle("tile");
-            tiles[j].setAttribute("onmouseover", "changeColor(this)");
-            tileSections[i].appendChild(tiles[j]);
-        }
-    }
-}
-
-function changeColor(element){
-    element.style.backgroundColor="black";
-    element.style.border = "1px solid rgb(31, 31, 31)"
-}
-
-// tiles[0].addEventListener("mouseover", changeColor);
-
-// function changeColor(){
-//     tiles[0].style.backgroundColor = "black";
-// }
-
-const container = document.querySelector(".container");
-
-const tileSections = [];
-const tiles = [];
+const container = document.querySelector(".container");  // Container for the grid of tiles
 
 const resetButton = document.querySelector(".resetButton");
 resetButton.addEventListener("click", reset);
@@ -58,4 +49,4 @@ changeSizeButton.addEventListener("click", changeSize);
 
 
 let size = 16;  // Default size
-draw();  // Draw initial grid
+createTiles();  // Draw initial grid
